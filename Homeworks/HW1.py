f9b11ecbcb765a47080605a231631993c1f7c5de
Kadir Efe Dilek <71722234+kadirefe-dilek@users.noreply.github.com>
2021-03-09 23:27:00 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,1 +1,19 @@
+nums = range(10)
+oddNums = list() #list that will hold only odd numbers
+evenNums = list() #list that will hold only even numbers
 
+for i in nums: #creating loop to assign correct numbers to each list
+  if i % 2 == 0: #defining even numbers
+    evenNums.append(i) #putting even numbers to related list
+  
+  else: #remaining case is numbers to be odd
+    oddNums.append(i) #putting odd numbers to the related list
+
+#print(oddNums)  #test line
+#print(evenNums) #test line
+  
+newList = list(oddNums + evenNums)  #declaring newList to be the merged list
+#print(newList) #test line
+for j in newList: #loop to do multiplication first then printing the type
+    j = j * 2
+    print(type(j))
